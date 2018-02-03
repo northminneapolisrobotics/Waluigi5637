d2dcf7711509ed46882ba63f390cffd58cf1c873
Lexie <klockhar.iit@gmail.com>
2018-02-02 18:55:15 -0600

    Some auto code

--- src/org/usfirst/frc/team2264/robot/Robot.java
+++ src/org/usfirst/frc/team2264/robot/Robot.java
@@ -1,120 +1,145 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
+ /*----------------------------------------------------------------------------*/
+ /* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
+ /* Open Source Software - may be modified and shared by FRC teams. The code   */
+ /* must be accompanied by the FIRST BSD license file in the root directory of */
+ /* the project.                                                               */
+ /*----------------------------------------------------------------------------*/
+ 
+ package org.usfirst.frc.team2264.robot;
 
-package org.usfirst.frc.team2264.robot;
+ import com.ctre.phoenix.motorcontrol.ControlMode;
 
-import com.ctre.phoenix.motorcontrol.ControlMode;
-import com.ctre.phoenix.motorcontrol.can.TalonSRX;
-
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.properties file in the
- * project.
- */
-
-public class Robot extends IterativeRobot {
-	private static final String kDefaultAuto = "Default";
-	private static final String kCustomAuto = "My Auto";
-	private String m_autoSelected;
-	private SendableChooser<String> m_chooser = new SendableChooser<>();
-	TalonSRX leftT = new TalonSRX(RobotMap.LEFT_MOTOR_PORT);
-	TalonSRX rightT = new TalonSRX(RobotMap.RIGHT_MOTOR_PORT);
-	Joystick joystickL = new Joystick(RobotMap.LEFT_JOYSTICK_PORT);
-	Joystick joystickR = new Joystick(RobotMap.RIGHT_JOYSTICK_PORT);
+ import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+ 
+ import edu.wpi.first.wpilibj.IterativeRobot;
+ import edu.wpi.first.wpilibj.Joystick;
+ import edu.wpi.first.wpilibj.Timer;
+ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+ import edu.wpi.first.wpilibj.command.TimedCommand;
+ /**
+  * The VM is configured to automatically run this class, and to call the
+  * functions corresponding to each mode, as described in the IterativeRobot
+  * documentation. If you change the name of this class or the package after
+  * creating this project, you must also update the build.properties file in the
+  * project.
+  */
+ 
+ public class Robot extends IterativeRobot {
+ 	private static final String kDefaultAuto = "Default";
+ 	private static final String kCustomAuto = "My Auto";
+ 	private String m_autoSelected;
+ 	private SendableChooser<String> m_chooser = new SendableChooser<>();
+ 	private double autoStartTime;
 	
-	
-	/**
-	 * This function is run when the robot is first started up and should be
-	 * used for any initialization code.
-	 */
+ 	TalonSRX leftT = new TalonSRX(RobotMap.LEFT_MOTOR_PORT);
+ 	TalonSRX rightT = new TalonSRX(RobotMap.RIGHT_MOTOR_PORT);
+ 	Joystick joystickL = new Joystick(RobotMap.LEFT_JOYSTICK_PORT);
+ 	Joystick joystickR = new Joystick(RobotMap.RIGHT_JOYSTICK_PORT);
+ 	
+ 	TimedCommand autoForward = new TimedCommand(RobotMap.AUTO_TIMER);
+ 	
+ 	/**
+ 	 * This function is run when the robot is first started up and should be
+ 	 * used for any initialization code.
+ 	 */
 	public Timer.Interface newTimer() {
 		return null;
 		
 	}
 	
-	@Override
-	public void robotInit() {
-		m_chooser.addDefault("Default Auto", kDefaultAuto);
-		m_chooser.addObject("My Auto", kCustomAuto);
-		SmartDashboard.putData("Auto choices", m_chooser);
-		
+ 	@Override
+ 	public void robotInit() {
+ 		m_chooser.addDefault("Default Auto", kDefaultAuto);
+ 		m_chooser.addObject("My Auto", kCustomAuto);
+ 		SmartDashboard.putData("Auto choices", m_chooser);
+ 		
+ 	}
+ 
+ 	/**
+ 	 * This autonomous (along with the chooser code above) shows how to select
+ 	 * between different autonomous modes using the dashboard. The sendable
+ 	 * chooser code works with the Java SmartDashboard. If you prefer the
+ 	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
+ 	 * getString line to get the auto name from the text box below the Gyro
+ 	 *
+ 	 * <p>You can add additional auto modes by adding additional comparisons to
+ 	 * the switch structure below with additional strings. If using the
+ 	 * SendableChooser make sure to add them to the chooser code above as well.
+ 	 */
+ 	@Override
+ 	public void autonomousInit() {
+ 		m_autoSelected = m_chooser.getSelected();
+ 		
+ 		// autoSelected = SmartDashboard.getString("Auto Selector",
+ 		// defaultAuto);
+ 		System.out.println("Auto selected: " + m_autoSelected);
+ 		
+ 		autoStartTime = System.currentTimeMillis();
+ 		
+ 	}
+ 
+ 	/**
+ 	 * This function is called periodically during autonomous.
+ 	 */
+ 	@Override
+ 	public void autonomousPeriodic() {
+ 		switch (m_autoSelected) {
+ 			case kCustomAuto:
+ 				// Put custom auto code here
+ 				if(System.currentTimeMillis() - autoStartTime <= 5000){
+ 		 			autoForward();
+ 		 		}
+ 		 		else {
+ 		 			autoStop();
+ 		 		}
+ 				
+ 				break;
+ 				
+ 			case kDefaultAuto:
+ 			default:
+ 				
+ 				if(System.currentTimeMillis() - autoStartTime <= 5000){
+ 		 			autoForward();
+ 		 		}
+ 		 		else {
+ 		 			autoStop();
+ 		 		}
+				//default auto code
+ 				break;
+ 				
+ 				
+ 		}
+ 	}
+ 	
+ 	private void autoStop() {
+ 		leftT.set(ControlMode.PercentOutput, 0);
+ 		rightT.set(ControlMode.PercentOutput, 0);
+ 	}
+ 	private void autoForward() {
+		// TODO Auto-generated method stub
+ 		leftT.set(ControlMode.PercentOutput, RobotMap.AUTO_MOTOR_ADJUST*RobotMap.MOTOR_FULL_ADJUST_LEFT*1);
+		rightT.set(ControlMode.PercentOutput, RobotMap.AUTO_MOTOR_ADJUST*RobotMap.MOTOR_FULL_ADJUST_RIGHT*1);
 	}
 
 	/**
-	 * This autonomous (along with the chooser code above) shows how to select
-	 * between different autonomous modes using the dashboard. The sendable
-	 * chooser code works with the Java SmartDashboard. If you prefer the
-	 * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-	 * getString line to get the auto name from the text box below the Gyro
-	 *
-	 * <p>You can add additional auto modes by adding additional comparisons to
-	 * the switch structure below with additional strings. If using the
-	 * SendableChooser make sure to add them to the chooser code above as well.
-	 */
-	@Override
-	public void autonomousInit() {
-		m_autoSelected = m_chooser.getSelected();
-		// autoSelected = SmartDashboard.getString("Auto Selector",
-		// defaultAuto);
-		System.out.println("Auto selected: " + m_autoSelected);
-	}
-
-	/**
-	 * This function is called periodically during autonomous.
-	 */
-	@Override
-	public void autonomousPeriodic() {
-		switch (m_autoSelected) {
-			case kCustomAuto:
-				// Put custom auto code here
-				break;
-			case kDefaultAuto:
-			default:
-				// sets the motors to half normal speed for autonomous to avoid damage
-					
-					if(){
-						
-					}
-					else{
-						leftT.set(ControlMode.PercentOutput, RobotMap.AUTO_MOTOR_ADJUST*RobotMap.MOTOR_FULL_ADJUST_LEFT*1);
-						rightT.set(ControlMode.PercentOutput, RobotMap.AUTO_MOTOR_ADJUST*RobotMap.MOTOR_FULL_ADJUST_RIGHT*1);
-					
-					}
-				// Put default auto code here
-				break;
-		}
-	}
-
-	/**
-	 * This function is called periodically during operator control.
-	 */
-	
-	@Override
-	public void teleopPeriodic() {
-		//puts the y number of the joysticks on the dashboard.
-		SmartDashboard.putNumber("Left Joystick" , joystickL.getY());
-		SmartDashboard.putNumber("Right Joystick" , joystickR.getY());
-		leftT.set(ControlMode.PercentOutput, RobotMap.MOTOR_FULL_ADJUST_LEFT*joystickL.getY());
-		rightT.set(ControlMode.PercentOutput, RobotMap.MOTOR_FULL_ADJUST_RIGHT*joystickR.getY());
-		
-	}
-
-	/**
-	 * This function is called periodically during test mode.
-	 */
-	@Override
-	public void testPeriodic() {
-	}
-}
+ 	 * This function is called periodically during operator control.
+ 	 */
+ 	
+ 	@Override
+ 	public void teleopPeriodic() {
+ 		//puts the y number of the joysticks on the dashboard.
+ 		SmartDashboard.putNumber("Left Joystick" , joystickL.getY());
+ 		SmartDashboard.putNumber("Right Joystick" , joystickR.getY());
+ 		leftT.set(ControlMode.PercentOutput, RobotMap.MOTOR_FULL_ADJUST_LEFT*joystickL.getY());
+ 		rightT.set(ControlMode.PercentOutput, 0.81*RobotMap.MOTOR_FULL_ADJUST_RIGHT*joystickR.getY());
+ 		
+ 	}
+ 
+ 	/**
+ 	 * This function is called periodically during test mode.
+ 	 */
+ 	@Override
+ 	public void testPeriodic() {
+ 	}
+ }
--- src/org/usfirst/frc/team2264/robot/RobotMap.java
+++ src/org/usfirst/frc/team2264/robot/RobotMap.java
@@ -1,4 +1,20 @@
-package org.usfirst.frc.team2264.robot;
+ package org.usfirst.frc.team2264.robot;
+ 
+ // maps out the ports and buttons and objects and things
+ // allows the changing of a variable only once, instead of multiple times 
+ public class RobotMap {
+ 	final static int LEFT_MOTOR_PORT = 2;
+ 	final static int RIGHT_MOTOR_PORT = 1;
+ 	final static int LEFT_JOYSTICK_PORT = 1;
+ 	final static int RIGHT_JOYSTICK_PORT = 0;
+	final static double MOTOR_SPEED = 0.5;
+	//final static double MOTOR_SPEED_AUTO = 0.5;
+ 	final static double MOTOR_ADJUST = .91;
+ 	final static int MOTOR_DIRECTION_LEFT = 1;
+ 	final static int MOTOR_DIRECTION_RIGHT = -1;
+ 	
+ 	final static double MOTOR_FULL_ADJUST_LEFT = RobotMap.MOTOR_DIRECTION_LEFT*RobotMap.MOTOR_SPEED;
+ 	final static double MOTOR_FULL_ADJUST_RIGHT = RobotMap.MOTOR_DIRECTION_RIGHT*RobotMap.MOTOR_ADJUST*RobotMap.MOTOR_SPEED;
 
 // maps out the ports and buttons and objects and things
 
@@ -16,7 +32,7 @@ public class RobotMap {
 	final static double MOTOR_FULL_ADJUST_RIGHT = RobotMap.MOTOR_DIRECTION_RIGHT*RobotMap.MOTOR_ADJUST*RobotMap.MOTOR_SPEED;
 
 	final static double AUTO_MOTOR_ADJUST = 0.5;
+	final static double AUTO_TIMER = 5;
 	
 	
-}
-
+ }
